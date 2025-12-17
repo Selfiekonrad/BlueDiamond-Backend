261aff1430260047032530b9567983da3b25b522
Konrad <konradceniuch@icloud.com>
2025-12-17 12:14:40 +0100

    init commit

---
+++ src/main/java/com/ceniuch/bluediamondbackend/BlueDiamondBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ceniuch.bluediamondbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueDiamondBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueDiamondBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ceniuch/bluediamondbackend/BlueDiamondBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceniuch.bluediamondbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueDiamondBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
